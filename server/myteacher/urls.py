46c1a83aeb1fff5b7399305327e6e04ff9f1b5a1
DorivalJunior23 <dorivaljunior23@outlook.com>
2022-10-09 01:16:06 -0300

    https://github.com/DorivalJunior23/MyTeacher

--- server/myteacher/urls.py
+++ server/myteacher/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from teacher.views import ProfessorAPIView
+from teacher.views import ProfessorAPIView, CadastrarAulaAPIView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('professores/', ProfessorAPIView.as_view())
+    path('professores/', ProfessorAPIView.as_view()),
+    path('professores/<int:id>/aulas', CadastrarAulaAPIView.as_view())
 ]
